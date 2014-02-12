edf8d0fe8af8a0654f3b1034729eb6bd7fbf6414
langloisga <gaston.langlois@ec.gc.ca>
2014-02-12 12:14:18 -0500

    Reporting Tool Installation Batch file

---
+++ InstallationSetUp.bat
@@ -0,0 +1,7 @@
+mkdir c:\QA-WS-Tool
+mkdir c:\QA-WS-Tool\WPSS_Tool
+mkdir c:\QA-WS-Tool\WPSS_Tool\results
+mkdir c:\Temp
+copy *.xls C:\QA-WS-Tool
+copy *.jar C:\QA-WS-Tool
+echo Installation Completed
